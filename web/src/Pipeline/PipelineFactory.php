40eeb32701e1e0c570d5076f58aef5182175e181
senira <senirahan@gmail.com>
2026-09-25 18:29:58 +0000

    FIX: PipelineFactory unused import HttpMethods

--- web/src/Pipeline/PipelineFactory.php
+++ web/src/Pipeline/PipelineFactory.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Vwork\Web\Pipeline;
 
-use Vwork\Web\Http\HttpMethods;
 use Vwork\Web\Registry\IHttpRegistry;
 use Vwork\Web\Controllers\IController;
 use Vwork\Web\Middleware\IMiddleware;
