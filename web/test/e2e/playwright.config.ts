33809c1d10a9841ce46cd8d6789f5d66319e7536
senira <senirahan@gmail.com>
2026-09-25 16:47:57 +0000

    FIX: (web/ (ts)) wrong import from 'playwright' instead of '@playwwright' & @types/node wasn't installed

--- web/test/e2e/playwright.config.ts
+++ web/test/e2e/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "playwright/test";
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
    testDir: ".",              // tests are IN this same folder — not "../tests/e2e", not "./tests/e2e"
